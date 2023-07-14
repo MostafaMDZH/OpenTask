70230035e47f6bc63834728de5e890410213f92c
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2023-07-14 20:56:14 +0330

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,6 @@
 import { ConfigProvider } from "antd"
 import { ThemeProvider } from "context/theme-provider"
-import { QueryClientProvider } from "@tanstack/react-query"
-import { queryClient } from 'utils'
 import '!style-loader!css-loader!sass-loader!../src/index.scss'
-import { initialize, mswDecorator } from 'msw-storybook-addon'
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { MmsHandlers } from "../src/utils/msw-handlers"
-import dayjs from 'dayjs'
-import updateLocale from 'dayjs/plugin/updateLocale'
-
-//start week from Monday:
-dayjs.extend(updateLocale)
-dayjs.updateLocale('en', {
-    weekStart: 1
-})
-
-//initialize MSW:
-initialize()
 
 //parameters:
 export const parameters = {
@@ -32,29 +16,22 @@ export const parameters = {
         values: [
             {
                 name: 'light',
-                // value: '#fff'
-                value: '#f5f5f5'
+                value: '#fff'
+                // value: '#f5f5f5'
             },
             {
                 name: 'dark',
                 value: '#333333'
             }
         ]
-    },
-    msw: {
-        handlers: MmsHandlers
     }
 }
 
 export const decorators = [
     Story =>
-        <QueryClientProvider client={queryClient}>
-            <ConfigProvider theme={{ token: { fontFamily: 'inter' } }}>
-                <ThemeProvider>
-                    <Story/>
-                    <ReactQueryDevtools/>
-                </ThemeProvider>
-            </ConfigProvider>
-        </QueryClientProvider>,
-    mswDecorator
+        <ConfigProvider theme={{ token: { fontFamily: 'Ubuntu' } }}>
+            <ThemeProvider>
+                <Story/>
+            </ThemeProvider>
+        </ConfigProvider>
 ]
