011a779c8845f0c1e56547d1e1e307c8572cf7ff
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2023-07-15 20:08:32 +0330

    Delete main-page.style.tsx

--- src/pages/task-list/main-page.style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const MainWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: ${props => `${props.theme.margin}px`}
-`
