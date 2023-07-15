908043e7b9cc6d781959b7a7b2a7752a19716783
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2023-07-15 20:09:37 +0330

    load the app

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,10 @@
-export const App = () => <></>
+import { ConfigProvider } from "antd"
+import { ThemeProvider } from "context/theme-provider"
+import { Main } from "pages/main/main"
+
+export const App = () =>
+    <ConfigProvider theme={{ token: { fontFamily: 'Ubuntu' } }}>
+        <ThemeProvider>
+            <Main/>
+        </ThemeProvider>
+    </ConfigProvider>
